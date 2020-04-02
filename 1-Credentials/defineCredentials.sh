0ba295283a26fed7ea2da05da8280890d7b20914
yul14nrc <61518345+yul14nrc@users.noreply.github.com>
2020-04-02 16:06:03 -0400

    Create defineCredentials.sh

---
+++ 1-Credentials/defineCredentials.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+CREDS=./creds.json
+rm $CREDS 2> /dev/null
+
+echo ""
+echo -e "${YLW}Please enter the credentials as requested below: ${NC}"
+echo ""
+read -p "Dynatrace Tenant ID (ex. https://<TENANT_ID>.live.dynatrace.com or https://<TENANT_ID>.dynatrace-managed.com): " DTTEN
+read -p "Dynatrace Environment ID (Dynatrace Managed Only - https://<TENANT_ID>.dynatrace-managed.com/e/<ENVIRONMENT_ID>): " DTENV
+read -p "Dynatrace API Token: " DTAPI
+read -p "Dynatrace PaaS Token: " DTPAAS
+
+echo ""
+echo -e "${YLW}Please confirm all are correct: ${NC}"
+echo ""
+echo "Dynatrace Tenant ID: $DTTEN"
+echo "Dynatrace Environment ID: $DTENV"
+echo "Dynatrace API Token: $DTAPI"
+echo "Dynatrace PaaS Token: $DTPAAS"
+
+echo ""
+read -p "Is this all correct? (y/n) : " -n 1 -r
+echo ""
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    rm $CREDS 2> /dev/null
+    cat ./creds.sav | sed 's~DYNATRACE_TENANT_ID~'"$DTTEN"'~' | \
+      sed 's~DYNATRACE_ENVIRONMENT_ID~'"$DTENV"'~' | \
+      sed 's~DYNATRACE_API_TOKEN~'"$DTAPI"'~' | \
+      sed 's~DYNATRACE_PAAS_TOKEN~'"$DTPAAS"'~' >> $CREDS
+fi
+echo ""
+cat $CREDS
+echo ""
+echo ""
+echo "The credentials file can be found here:" $CREDS
+echo ""

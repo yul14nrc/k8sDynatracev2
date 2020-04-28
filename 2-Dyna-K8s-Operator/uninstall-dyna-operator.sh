0af6a379b651314f29ea328571ec821be2183e0e
yul14nrc <61518345+yul14nrc@users.noreply.github.com>
2020-04-28 12:21:09 -0500

    Update and rename unistall-dyna-operator.sh to uninstall-dyna-operator.sh

---
+++ 2-Dyna-K8s-Operator/uninstall-dyna-operator.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+exec > >(tee -i ./uninstalldynaoperator.log)
+exec 2>&1
+
+kubectl delete -n dynatrace oneagent --all
+
+dynaoperator=`kubectl get deployment dynatrace-oneagent-operator -n dynatrace -o=jsonpath='{$.spec.template.spec.containers[:1].image}'`
+version=$(echo "$dynaoperator" | cut -d':' -f 2)
+
+kubectl delete -f https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/$version/deploy/kubernetes.yaml
--- 2-Dyna-K8s-Operator/unistall-dyna-operator.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kubectl delete -n dynatrace oneagent --all
-
-LATEST_RELEASE=$(curl -s https://api.github.com/repos/dynatrace/dynatrace-oneagent-operator/releases/latest | grep tag_name | cut -d '"' -f 4)
-#LATEST_RELEASE=v0.3.1
-kubectl delete -f https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/$LATEST_RELEASE/deploy/kubernetes.yaml
-
-kubectl delete namespace dynatrace
